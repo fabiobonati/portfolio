e526666c846dd5fa70ae2243698c1bbb375c080a
fabiobonati <bonatifabio.dev@gmail.com>
2023-06-01 23:49:01 +0200

    Basic SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Head>
+    <title>Fabio Bonati</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta
+      name='description'
+      content='Fabio Bonati, 19 y/o developer and student'
+    />
+    <meta name='author' content='Fabio Bonati' />
+  </Head>;
+  return <Component {...pageProps} />;
 }
