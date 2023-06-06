28222c5c9e378c0bbc04e790bc915177d4643da7
fabiobonati <bonatifabio.dev@gmail.com>
2023-06-06 18:26:07 +0200

    Minor fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
